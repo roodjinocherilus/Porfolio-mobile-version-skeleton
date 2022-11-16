4f202177365f9d386d12ccc0746acbd02f5d909d
Roodjino Cherilus <cherilusjcr@gmail.com>
2022-11-16 01:37:50 +0000

    fix linter error

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ function on() {
 function off() {
   document.getElementById('overlay').style.display = 'none';
 }
+document.querySelector('#menu').addEventListener('click', () => on());
+document.querySelector('.close-icon').addEventListener('click', () => off());
+document.querySelectorAll('.text-a').forEach((item) => {
+  item.addEventListener('click', () => off());
+});
